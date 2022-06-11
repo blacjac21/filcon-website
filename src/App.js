e69030252d0100d349f49f561fa23a9611d7deb7
blacjac21 <blacjac21@gmail.com>
2022-06-11 11:07:24 +0530

    mui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button } from '@mui/material';
+import { useEffect } from 'react';
 
 function App() {
+
+  useEffect(() => {
+    document.title = "Filcon - Home";
+  },[]);
+
+
+
   return (
-    <div className="App12">
-      
+    <div className="App">
+      <Button>Test1</Button>
     </div>
   );
 }
