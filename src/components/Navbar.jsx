d6344f2b94496a981c40fd7f027398fd30215fef
blacjac21 <blacjac21@gmail.com>
2022-06-21 19:30:10 +0530

    gg

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,27 @@
-import { AppBar, Toolbar } from "@mui/material";
+import { AppBar, Toolbar, Typography } from "@mui/material";
 import React from "react";
+import { styled } from "@mui/material";
+import { Colors } from "../styles/theme";
 
 const StyledToolbar = styled(Toolbar)({
     display:"flex",
     justifyContent:"space-between"
 })
 
+const NavTypo = styled(Typography)({
+    color: Colors.white,
+    fontFamily: '"Montserrat", "sans-serif" '
+
+})
+
 
 
 const Navbar = () => {
     return (
-        <AppBar position="sticky">
-            <Toolbar>
-              NavBar
-            </Toolbar>
+        <AppBar position="static">
+            <StyledToolbar>
+              <NavTypo>Filcon Sales & Service</NavTypo>
+            </StyledToolbar>
         </AppBar>
     )
 }
