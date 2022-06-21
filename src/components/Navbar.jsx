04deb5a5e57f23b8417cda77ad80b5dcda5c8536
blacjac21 <blacjac21@gmail.com>
2022-06-21 20:58:23 +0530

    prog

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,13 @@
-import { AppBar, Toolbar, Typography } from "@mui/material";
-import React from "react";
+import { AppBar, createTheme, IconButton, Toolbar, Typography, useTheme } from "@mui/material";
+import React, { Component } from "react";
 import { styled } from "@mui/material";
 import { Colors } from "../styles/theme";
+import { Box } from "@mui/material";
+import logo from "../assets/logo.png"
+import { MenuRounded  } from "@mui/icons-material";
+import { fontSize } from "@mui/system";
+
+
 
 const StyledToolbar = styled(Toolbar)({
     display:"flex",
@@ -10,20 +16,75 @@ const StyledToolbar = styled(Toolbar)({
 
 const NavTypo = styled(Typography)({
     color: Colors.white,
-    fontFamily: '"Montserrat", "sans-serif" '
+    fontFamily: '"Montserrat", "sans-serif" ',
+    
+})
+
+const Logo = styled(Box)({
+    height:  '2.5em' ,
+    width: '2.5em' ,
+          /*
+          maxHeight: { xs: 233, md: 167 },
+          maxWidth: { xs: 350, md: 250 },
+          */
+          paddingTop: '0.5em',
+          paddingBottom: '0.5em'
+          
 
 })
 
+const Ham = styled(MenuRounded)({
+    color : Colors.white,
+    color : Colors.secondary ,
+    fontSize : "2em"
+})
 
 
 const Navbar = () => {
     return (
         <AppBar position="static">
             <StyledToolbar>
-              <NavTypo>Filcon Sales & Service</NavTypo>
+                <Logo component="img" src={logo} />
+             
+              <NavTypo >Filcon Sales & Service</NavTypo>
+
+                <Ham/>
+                   
             </StyledToolbar>
         </AppBar>
     )
 }
 
 export default Navbar
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+<Box
+        component="img"
+        sx={{
+          height:  '2.5em' ,
+          width: '2.5em' ,
+          
+          maxHeight: { xs: 233, md: 167 },
+          maxWidth: { xs: 350, md: 250 },
+          
+
+          paddingTop: '0.5em',
+          paddingBottom: '0.5em'
+        }}
+        src={logo}
+      /> 
+      */
+
