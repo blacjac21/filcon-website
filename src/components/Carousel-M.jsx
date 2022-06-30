a129544e5c2742fab7a685a39c824227116d7203
blacjac21 <blacjac21@gmail.com>
2022-06-30 22:10:11 +0530

    Carous done

--- src/components/Carousel-M.jsx
+++ src/components/Carousel-M.jsx
@@ -7,7 +7,7 @@ import "../styles/Carousel.css"
 
 const CarouselM = () => {
     return (
-        <AliceCarousel autoPlay autoPlayInterval="100" animationType="slide" disableButtonsControls disableDotsControls>
+        <AliceCarousel autoPlay autoPlayInterval="3000" infinite animationType="slide" disableButtonsControls disableDotsControls>
             <img src={photo1} className="sliderimg"/>
             <img src={photo2} className="sliderimg"/>
 
