04ded84660ca9aaed5db542ed408041b89aefb28
blacjac21 <blacjac21@gmail.com>
2022-06-30 17:47:49 +0530

    finally

--- src/components/Carousel-M.jsx
+++ src/components/Carousel-M.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import AliceCarousel from "react-alice-carousel";
 import photo1 from "../assets/1.jpg";
 import photo2 from "../assets/2.jpg";
+import "react-alice-carousel/lib/alice-carousel.css";
+import "../styles/Carousel.css"
 
 const CarouselM = () => {
     return (
-        <AliceCarousel autoPlay autoPlayInterval="3000" infinite animationType="slide"  height="2em"  disableButtonsControls disableDotsControls>
+        <AliceCarousel autoPlay autoPlayInterval="100" animationType="slide" disableButtonsControls disableDotsControls>
             <img src={photo1} className="sliderimg"/>
-             <img src={photo2} className="sliderimg"/>
+            <img src={photo2} className="sliderimg"/>
 
         </AliceCarousel>
         
